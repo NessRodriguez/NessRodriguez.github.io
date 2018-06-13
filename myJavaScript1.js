0ca4fbbd52f753e469e3e3bc92f50d331cbb7bf4
NessRodriguez <rodriguez.vanessa.842@gmail.com>
2018-06-13 13:35:13 -0700

    Add files via upload

---
+++ myJavaScript1.js
@@ -0,0 +1,36 @@
+function myFunc(){
+  document.getElementById("demo").innerHTML = "Please";
+}
+
+var today= new Date()
+
+anime.timeline({loop: true})
+  .add({
+    targets: '.ml11 .line',
+    scaleY: [0,1],
+    opacity: [0.5,1],
+    easing: "easeOutExpo",
+    duration: 700
+  })
+  .add({
+    targets: '.ml11 .line',
+    translateX: [0,$(".ml11 .letters").width()],
+    easing: "easeOutExpo",
+    duration: 700,
+    delay: 100
+  }).add({
+    targets: '.ml11 .letter',
+    opacity: [0,1],
+    easing: "easeOutExpo",
+    duration: 600,
+    offset: '-=775',
+    delay: function(el, i) {
+      return 34 * (i+1)
+    }
+  }).add({
+    targets: '.ml11',
+    opacity: 0,
+    duration: 1000,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
